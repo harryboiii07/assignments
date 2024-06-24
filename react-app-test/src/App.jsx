0e39e9b2625fa4942099d0a7651a5d57d76d04ac
harshit <chicholikarharshit@gmail.com>
2024-06-25 01:43:15 +0530

    learning use context

--- react-app-test/src/App.jsx
+++ react-app-test/src/App.jsx
@@ -1,19 +1,46 @@
-import { useCallback, useEffect, useRef, useState } from 'react'
+import { useCallback, useContext, useEffect, useRef, useState } from 'react'
+import { Countcontext } from './context';
 
 function App() {
-  const divref = useRef();
-
-  useEffect(() => {
-    setTimeout(() => {
-      divref.current.innerhtml = 10;
-    }, 5000);
-  }, [])
-
-  return (
+  const [count,setcount] = useState(0);
+  return(
     <div>
-        hi there, your income tax returns are <div ref={divref}>{incomeTax}</div>
+      <Countcontext.Provider value = {{count,setcount}}>
+      <Count />
+      </Countcontext.Provider>
     </div>
   )
 }
+
+function Count(){
+  return(
+    <div>
+      <Countrenderer />
+      <Buttons/>
+    </div>
+  )
+}
+
+function Countrenderer(){
+  const {count} = useContext(Countcontext);
+  return <div>
+      {count}
+    </div>
+}
+function Buttons(){
+  const {count,setcount} = useContext(Countcontext);
+  return(
+    <div>
+      <button onClick={()=>{
+        setcount(count+1)
+      }}>increase</button>
+
+      <button onClick={()=>{
+        setcount(count-1)
+      }}>decrease</button>
+    </div>
+  )
+}
+
 
 export default App
---
+++ react-app-test/src/context.jsx
@@ -0,0 +1,7 @@
+import { createContext } from "react";
+
+export const Countcontext = createContext({
+  count : 0,
+  setcount : ()=>{}
+});
+
